b6872b9204075b16c9375d1bd228183ac2fa1e55
jasper <ejg11@sfu.ca>
2022-11-07 17:18:05 -0800

    Interactive Single Product Showcase
    Single products page has a functioning interactive image display.

--- P02_Website/scripts/main.js
+++ P02_Website/scripts/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Code adapted from https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+  }
+
+  
