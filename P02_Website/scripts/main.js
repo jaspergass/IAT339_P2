e5fb69ae90315aa400a8011f4b0b0321ab9843c8
jasper <ejg11@sfu.ca>
2022-11-11 09:14:37 -0800

    Cart quantity and remove buttons
    added things to the cart, needs a little bit of styling/reorganization. added a normalize css file since we didnt have one.

--- P02_Website/scripts/main.js
+++ P02_Website/scripts/main.js
@@ -7,3 +7,20 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
   }
 
+// Product quantity counter script. from https://dev.to/stackfindover/products-quantity-counter-using-html-css-javascript-663
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10); 
+  value = isNaN(value)? 0 : value;
+  value ++;
+  input.value = value;
+}
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10); 
+  if (value > 1) {
+    value = isNaN(value)? 0 : value;
+    value --;
+    input.value = value;
+  }
+}
