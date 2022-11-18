b3a57d50c04c92454f6bd9d3adfbac4c4b39f9a2
Christy Fang <yfa37@sfu.ca>
2022-11-18 10:41:06 -0800

    Style Guide Update 1
    Update latest layout

--- P02_Website/scripts/main.js
+++ P02_Website/scripts/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Code adapted from https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
 function myFunction(imgs) {
@@ -10,14 +10,15 @@ function myFunction(imgs) {
 // Product quantity counter script. from https://dev.to/stackfindover/products-quantity-counter-using-html-css-javascript-663
 function increaseCount(a, b) {
   var input = b.previousElementSibling;
-  var value = parseInt(input.value, 10); 
+  var value = parseInt(input.value, 10);
   value = isNaN(value)? 0 : value;
   value ++;
   input.value = value;
 }
+
 function decreaseCount(a, b) {
   var input = b.nextElementSibling;
-  var value = parseInt(input.value, 10); 
+  var value = parseInt(input.value, 10);
   if (value > 1) {
     value = isNaN(value)? 0 : value;
     value --;
