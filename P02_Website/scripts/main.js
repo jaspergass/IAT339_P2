39506786269bc39ef912d40b71045d4e9e04deff
jasper <ejg11@sfu.ca>
2022-11-17 22:58:15 -0800

    Citations & Validated
    Finished citations and made small edits after running the files through validators

--- P02_Website/scripts/main.js
+++ P02_Website/scripts/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Code adapted from https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
 function myFunction(imgs) {
@@ -25,63 +25,4 @@ function decreaseCount(a, b) {
   }
 }
 
-// Rotating Banner
-var bannerStatus = 1;
-var bannerTimer = 4000;
-
-window.onload = function(){
-  bannerLoop();
-}
-
-var startBannerLoop = setInterval(function(){
-  bannerLoop();
-}, bannerTimer);
-
-function bannerloop(){
-  if (bannerStatus === 1){
-    document.getElementById("imgban2").style.opacity = "0";
-    setTimeout(function(){
-      document.getElementById("imgban1").style.right = "0px";
-      document.getElementById("imgban1").style.zIndex = "1000";
-      document.getElementById("imgban2").style.right = "-1200px";
-      document.getElementById("imgban2").style.zIndex = "1500";
-      document.getElementById("imgban3").style.right = "1200px";
-      document.getElementById("imgban3").style.zIndex = "500";
-    }, 500);
-    setTimeout(function(){
-      document.getElementById("imgban2").style.opacity = "1";
-    }, 1000);
-    bannerStatus = 2;
-  }
-  else if (bannerStatus === 2){
-    document.getElementById("imgban3").style.opacity = "0";
-    setTimeout(function(){
-      document.getElementById("imgban2").style.right = "0px";
-      document.getElementById("imgban2").style.zIndex = "1000";
-      document.getElementById("imgban3").style.right = "-1200px";
-      document.getElementById("imgban3").style.zIndex = "1500";
-      document.getElementById("imgban1").style.right = "1200px";
-      document.getElementById("imgban1").style.zIndex = "500";
-    }, 500);
-    setTimeout(function(){
-      document.getElementById("imgban3").style.opacity = "1";
-    }, 1000);
-    bannerStatus = 3;
-  }
-  else if (bannerStatus === 3){
-    document.getElementById("imgban1").style.opacity = "0";
-    setTimeout(function(){
-      document.getElementById("imgban3").style.right = "0px";
-      document.getElementById("imgban3").style.zIndex = "1000";
-      document.getElementById("imgban1").style.right = "-1200px";
-      document.getElementById("imgban1").style.zIndex = "1500";
-      document.getElementById("imgban2").style.right = "1200px";
-      document.getElementById("imgban2").style.zIndex = "500";
-    }, 500);
-    setTimeout(function(){
-      document.getElementById("imgban1").style.opacity = "1";
-    }, 1000);
-    bannerStatus = 1;
-  }
-}
 
